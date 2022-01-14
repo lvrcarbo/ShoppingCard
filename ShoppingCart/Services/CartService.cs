d482ba43613a207162a4c386c60daa05067fd559
Alvaro Carbo <lvrcarbo@gmail.com>
2022-01-14 11:59:45 +0100

    Chapter 6 DONE

---
+++ ShoppingCart/Services/CartService.cs
@@ -0,0 +1,30 @@
+﻿using ShoppingCart.Models;
+
+namespace ShoppingCart.Services
+{
+    public class CartService : ICartService
+    {
+        public CartService() { Cart = new List<Product>(); }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        public IList<Product> Cart { get; private set; }
+
+        public int Total { get; set; }
+
+        public event Action OnChange;
+
+        public void AddProduct(Product product)
+        {
+            Cart.Add(product);
+            Total += product.Price;
+            NotifyStateChanged();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            Cart.Remove(product);
+            Total -= product.Price;
+            NotifyStateChanged();
+        }
+    }
+}
